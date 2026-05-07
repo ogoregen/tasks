8c70593197f70228ee44e62935af744605912a6f
ogoregen <37447279+ogoregen@users.noreply.github.com>
2026-05-07 17:13:05 +0300

    Initial upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const CACHE = 'dashboard-v1';
+const SHELL = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
