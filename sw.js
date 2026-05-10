2549418dd33467a759e1b95e83d934775fc128d3
ogoregen <37447279+ogoregen@users.noreply.github.com>
2026-05-10 21:20:08 +0300

    Bump SW cache to v9

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'dashboard-v8';
+const CACHE = 'dashboard-v9';
 const SHELL = ['./index.html', './app.js', './style.css', './manifest.json', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
