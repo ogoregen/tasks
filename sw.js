216f7b8d0599b563c6adee7c3262e5f14cc39daf
ogoregen <37447279+ogoregen@users.noreply.github.com>
2026-05-11 21:50:40 +0300

    Redesign icon: single centered white square, bump SW to v16

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'dashboard-v15';
+const CACHE = 'dashboard-v16';
 const SHELL = ['./index.html', './app.js', './style.css', './manifest.json', './icons/icon.svg', './icons/icon-maskable.svg', './icons/icon-maskable-512.png', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
